ad8a6225e2f657746b3444cdc8f5b4f3ce08be31
Artyom Eremchuk <artyom.eremchuk@gmail.com>
2024-02-27 19:37:19 +0300

    add mjs as js folder

---
+++ mjs_as_js/index.js
@@ -0,0 +1,27 @@
+import { Square as SquareClassic } from "./square.js";
+import Square from "./squareDefault.js";
+
+const squareDefault = new Square(3);
+const squareClassic = new SquareClassic(4);
+
+console.log(`SquareDefault ${squareDefault.area()}`);
+console.log(`SquareClassic ${squareClassic.area()}`);
+
+const p1 = new Promise((resolve, reject) => {
+  console.log("timeout started");
+  setTimeout(() => {
+    console.log("timeout finished");
+    if (Boolean(+new Date().getSeconds() % 2)) {
+      resolve("success");
+    } else {
+      reject("error");
+    }
+  }, 2000);
+});
+
+try {
+  const successResult = await p1;
+  console.log("Async call success: ", successResult);
+} catch (e) {
+  console.log("Async call failed: ", e);
+}
---
+++ mjs_as_js/square.js
@@ -0,0 +1,9 @@
+export class Square {
+  constructor(w) {
+    this.w = w;
+  }
+
+  area() {
+    return this.w * this.w;
+  }
+}
